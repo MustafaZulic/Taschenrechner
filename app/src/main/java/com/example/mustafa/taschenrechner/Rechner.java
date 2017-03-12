e4d9817c518c472a96e09a151e47ec9339543c75
Mustafa Zulic <mustafa.zulic93@gmail.com>
2017-03-12 18:24:37 +0100

    Default Version

---
+++ app/src/main/java/com/example/mustafa/taschenrechner/Rechner.java
@@ -0,0 +1,13 @@
+package com.example.mustafa.taschenrechner;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Rechner extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.meinrechner);
+    }
+}
