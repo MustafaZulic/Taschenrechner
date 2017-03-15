460a993426f172d866607f50d3d1cff8ad9ae436
Mustafa Zulic <mustafa.zulic93@gmail.com>
2017-03-15 14:39:24 +0100

    GUI neu erstellt und Sachen ausprobiert

--- app/src/main/java/com/example/mustafa/taschenrechner/Rechner.java
+++ app/src/main/java/com/example/mustafa/taschenrechner/Rechner.java
@@ -2,12 +2,25 @@ package com.example.mustafa.taschenrechner;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class Rechner extends AppCompatActivity {
+
+    EditText diviZahl1;
+    EditText diviZahl2;
+    Button auführen;
+    TextView ergebnis;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.meinrechner);
+
+        diviZahl1 = (EditText)
+
     }
 }
